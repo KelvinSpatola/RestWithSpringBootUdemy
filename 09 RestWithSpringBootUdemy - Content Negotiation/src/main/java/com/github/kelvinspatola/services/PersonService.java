a40b302b8a74c9e6e39682f6a082a100a9a55d23
Kelvin Spatola <kelvin.clark@ua.pt>
2022-06-14 09:56:38 +0100

    Session 13 started

---
+++ 09 RestWithSpringBootUdemy - Content Negotiation/src/main/java/com/github/kelvinspatola/services/PersonService.java
@@ -0,0 +1,55 @@
+package com.github.kelvinspatola.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.github.kelvinspatola.converter.DozerConverter;
+import com.github.kelvinspatola.data.model.Person;
+import com.github.kelvinspatola.data.vo.v1.PersonVO;
+import com.github.kelvinspatola.repository.PersonRepository;
+
+@Service
+public class PersonService {
+	
+	@Autowired
+	PersonRepository repository;
+	
+	public PersonVO create(PersonVO person) {
+		var entity = DozerConverter.parseObject(person, Person.class);
+		var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+		return vo;
+	}
+		
+	public List<PersonVO> findAll() {
+		return DozerConverter.parseListObjects(repository.findAll(), PersonVO.class);
+	}
+	
+	public PersonVO findById(Long id) {
+		var entity =  repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this id!"));
+		
+		return DozerConverter.parseObject(entity, PersonVO.class);
+	}
+	
+	public PersonVO update(PersonVO person) {
+		var entity = repository.findById(person.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this id!"));
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		return DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+	}
+	
+	public void delete(Long id) {
+		Person entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this id!"));
+		
+		repository.delete(entity);
+	}
+}
