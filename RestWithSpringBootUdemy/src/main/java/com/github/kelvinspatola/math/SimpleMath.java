cfe5f8a6dc9fa26dba0e0fed5d53a08d26809fba
Kelvin Spatola <kelvin.clark@ua.pt>
2022-06-21 10:53:59 +0100

    Session finished

---
+++ RestWithSpringBootUdemy/src/main/java/com/github/kelvinspatola/math/SimpleMath.java
@@ -0,0 +1,31 @@
+package com.github.kelvinspatola.math;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimpleMath {
+	
+	public Double sum(Double a, Double b) {
+		return a + b;
+	}
+	
+	public Double sub(Double a, Double b) {
+		return a - b;
+	}
+	
+	public Double mult(Double a, Double b) {
+		return a * b;
+	}
+	
+	public Double div(Double a, Double b) {
+		return a / b;
+	}
+	
+	public Double avg(Double a, Double b) {
+		return sum(a, b) / 2;
+	}
+	
+	public Double sqrt(Double a) {
+		return Math.sqrt(a);
+	}
+}
